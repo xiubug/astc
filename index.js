616dfaf6a62ccaf417997883c34d1bc9272e7059
sosout <sosout@139.com>
2018-09-14 16:43:35 +0800

    style(init astc): init astc
    init astc

--- index.js
+++ index.js
@@ -193,7 +193,168 @@ function parser(tokens) {
   return ast;
 }
 
+// 遍历器
+function traverser(ast, visitor) {
+  // 遍历 AST节点数组 对数组中的每一个元素调用 `traverseNode` 函数。
+  function traverseArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent);
+    });
+  }
+
+  // 接受一个 `node` 和它的父节点 `parent` 作为参数
+  function traverseNode(node, parent) {
+    // 从 visitor 获取对应方法的对象
+    let methods = visitor[node.type];
+    // 通过 visitor 对应方法操作当前 node
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      // 根节点
+      case 'Program':
+        traverseArray(node.body, node);
+        break;
+      // 函数调用
+      case 'CallExpression':
+        traverseArray(node.params, node);
+        break;
+      // 数值和字符串，不用处理
+      case 'NumberLiteral':
+      case 'StringLiteral':
+        break;
+
+      // 无法识别的字符，抛出错误提示
+      default:
+        throw new TypeError(node.type);
+    }
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+  // 开始遍历
+  traverseNode(ast, null);
+}
+
+// 转化器，参数：AST
+function transformer(ast) {
+  // 创建 `newAST`，它与之前的 AST 类似，Program：新AST的根节点
+  let newAst = {
+    type: 'Program',
+    body: [],
+  };
+
+  // 通过 _context 维护新旧 AST，注意 _context 是一个引用，从旧的 AST 到新的 AST。
+  ast._context = newAst.body;
+
+  // 通过遍历器遍历 参数：AST 和 visitor
+  traverser(ast, {
+    // 数值，直接原样插入新AST
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'NumberLiteral',
+          value: node.value,
+        });
+      },
+    },
+    // 字符串，直接原样插入新AST
+    StringLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'StringLiteral',
+          value: node.value,
+        });
+      },
+    },
+    // 函数调用
+    CallExpression: {
+      enter(node, parent) {
+        // 创建不同的AST节点
+        let expression = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name,
+          },
+          arguments: [],
+        };
+
+        // 函数调用有子类，建立节点对应关系，供子节点使用
+        node._context = expression.arguments;
+
+        // 顶层函数调用算是语句，包装成特殊的AST节点
+        if (parent.type !== 'CallExpression') {
+
+          expression = {
+            type: 'ExpressionStatement',
+            expression: expression,
+          };
+        }
+        parent._context.push(expression);
+      },
+    }
+  });
+  return newAst;
+}
+
+// 代码生成器 参数：新 AST
+function codeGenerator(node) {
+
+  switch (node.type) {
+    // 遍历 body 属性中的节点，且递归调用 codeGenerator，结果按行输出
+    case 'Program':
+      return node.body.map(codeGenerator)
+        .join('\n');
+
+    // 表达式，处理表达式内容，并用分号结尾
+    case 'ExpressionStatement':
+      return (
+        codeGenerator(node.expression) +
+        ';'
+      );
+
+    // 函数调用，添加左右括号，参数用逗号隔开
+    case 'CallExpression':
+      return (
+        codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator)
+          .join(', ') +
+        ')'
+      );
+
+    // 标识符，数值，原样输出
+    case 'Identifier':
+      return node.name;
+    case 'NumberLiteral':
+      return node.value;
+
+    // 字符串，用双引号包起来再输出
+    case 'StringLiteral':
+      return '"' + node.value + '"';
+
+    // 无法识别的字符，抛出错误提示
+    default:
+      throw new TypeError(node.type);
+  }
+}
+
+function compiler(input) {
+  let tokens = tokenizer(input);
+  let ast    = parser(tokens);
+  let newAst = transformer(ast);
+  let output = codeGenerator(newAst);
+
+  return output;
+}
+
 module.exports = {
   tokenizer,
-  parser
+  parser,
+  traverser,
+  transformer,
+  codeGenerator,
+  compiler,
 };
--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const {
   tokenizer,
   parser,
+  transformer,
+  codeGenerator,
+  compiler,
 } = require('./index');
 const assert = require('assert');
 
 const input  = '(add 2 (subtract 4 2))';
+const output = 'add(2, subtract(4, 2));';
 
 const tokens = [
   { type: 'paren',  value: '('        },
@@ -40,7 +44,41 @@ const ast = {
   }]
 };
 
+const newAst = {
+  type: 'Program',
+  body: [{
+    type: 'ExpressionStatement',
+    expression: {
+      type: 'CallExpression',
+      callee: {
+        type: 'Identifier',
+        name: 'add'
+      },
+      arguments: [{
+        type: 'NumberLiteral',
+        value: '2'
+      }, {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          name: 'subtract'
+        },
+        arguments: [{
+          type: 'NumberLiteral',
+          value: '4'
+        }, {
+          type: 'NumberLiteral',
+          value: '2'
+        }]
+      }]
+    }
+  }]
+};
+
 assert.deepStrictEqual(tokenizer(input), tokens, 'Tokenizer should turn `input` string into `tokens` array');
 assert.deepStrictEqual(parser(tokens), ast, 'Parser should turn `tokens` array into `ast`');
+assert.deepStrictEqual(transformer(ast), newAst, 'Transformer should turn `ast` into a `newAst`');
+assert.deepStrictEqual(codeGenerator(newAst), output, 'Code Generator should turn `newAst` into `output` string');
+assert.deepStrictEqual(compiler(input), output, 'Compiler should turn `input` into `output`');
 
 console.log('All Passed!');
